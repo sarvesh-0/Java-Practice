0f7b395d42f22e1166f4ada5336cf6cbc1b31f49
sarvesh-0 <sarveshteware651@gmail.com>
2024-09-24 15:18:49 +0530

    Chair Class Commit

---
+++ CoreJavaBook1/src/day2HW/Chair.java
@@ -0,0 +1,46 @@
+package day2HW;
+
+public class Chair {
+	String color ;
+	String chairType ;
+	int chairNo ;
+	boolean foaldable;
+	
+	public void setColor(String c) {
+		color = c;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setChairType(String ct) {
+		chairType = ct;
+	}
+	public String getChairType() {
+		return chairType;
+	}
+	public void setChairNo(int cs) {
+		chairNo = cs;
+	}
+	public int getChairNo() {
+		return chairNo;
+	}
+	public void setFoaldable(boolean f) {
+		foaldable = f;
+	}
+	private boolean getFoaldable() {
+		return foaldable ;
+	}
+	
+	public static void main(String[] args) {
+		Chair c1 = new Chair();
+		c1.setChairNo(15);
+		c1.setChairType("Steel");
+		c1.setColor("Black");
+		c1.setFoaldable(true);
+		
+		System.out.println("ChairNo : " +c1.getChairNo());
+		System.out.println("Chair Type : " +c1.getChairType());
+		System.out.println("Chair Color : " +c1.getColor());
+		System.out.println("Foaldable : " +c1.getFoaldable());
+}
+}
