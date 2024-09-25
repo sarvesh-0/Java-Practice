cbde7a696e05b5e99aeb387ac6f91efdd06567b7
sarvesh-0 <sarveshteware651@gmail.com>
2024-09-25 15:36:38 +0530

    isEven method

---
+++ CoreJavaBook1/src/day3Hw/EvenOdd.java
@@ -0,0 +1,30 @@
+package day3Hw;
+/*
+ Write a method to check whether number is even or odd and print appropriate message.
+ */
+import java.util.Scanner;
+public class EvenOdd {
+	
+	public static boolean isEven(int num) {
+		if(num%2==0) {
+			return true;
+		}
+		else {
+			return false ;
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Number : ");
+		int no = sc.nextInt();
+		sc.close();
+		boolean res = isEven(no);
+		if(res) {
+			System.out.println(no+" is Even !");
+		}
+		else {
+			System.out.println(no+" is Odd !");
+		}
+	}
+}
