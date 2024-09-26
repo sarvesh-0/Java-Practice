6733604099ab62bad5deaceccdc2bbb19d77122d
sarvesh-0 <sarveshteware651@gmail.com>
2024-09-26 20:46:18 +0530

    For each demo

---
+++ CoreJavaBook1/src/day4_1/ForEachLoopDemo.java
@@ -0,0 +1,14 @@
+package day4_1;
+
+public class ForEachLoopDemo 
+{
+ public static void main(String[] args) 
+ {
+   int num[]={10,20,30,40,50};
+   System.out.println("Array Elements are:");
+   for(int x:num)
+   {
+	   System.out.println(" "+x);
+   }
+  }//end main
+}//end class
