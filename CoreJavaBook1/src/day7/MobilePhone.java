63b67fdf6bc648d6f1dae79f63a558f03e2d619b
sarvesh-0 <sarveshteware651@gmail.com>
2024-09-30 14:35:19 +0530

    Mobile phone

--- CoreJavaBook1/src/day7/MobilePhone.java
+++ CoreJavaBook1/src/day7/MobilePhone.java
@@ -1,5 +1,13 @@
 package day7;
 
+/*
+ * Write an abstract class MobilePhone with abstract method makeCall().
+Inherit above class using class SimplePhone and override makeCall() method for printing audio calling.
+Create another subclass SmartPhone of MobilePhone and override makeCall() method to print video calling.
+Write a class Test with main() method. Using superclass MobilePhone reference create object of subclasses 
+SimplePhone and SmartPhone and invoke makeCall() method.
+ */
+
 public abstract class MobilePhone {
 	public abstract void makeCall();
 }
