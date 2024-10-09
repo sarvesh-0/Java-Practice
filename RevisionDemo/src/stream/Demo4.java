cfcbfa4e8e6fed48fabfe5749b7e60ecb111ce17
sarvesh-0 <sarveshteware651@gmail.com>
2024-10-09 14:48:29 +0530

    Stream Demo 4

--- RevisionDemo/src/stream/Demo4.java
+++ RevisionDemo/src/stream/Demo4.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 public class Demo4 {
 	public static void main(String[] args) {
 		List<Integer> list = new ArrayList<>(Arrays.asList(1,2,3,4,5));
-		List<Integer> res = list.stream()
+		List<Integer> res  = list.stream()
 						.filter(val -> val % 2 == 0)
 						.peek(val -> System.out.println("Filtered : "+val))
 						.map(val -> val * 10)
