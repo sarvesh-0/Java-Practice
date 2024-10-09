f3e1bd495471cfe8d6445b11ad42b9ef6f7b7fca
sarvesh-0 <sarveshteware651@gmail.com>
2024-10-09 14:33:11 +0530

    Stream Demo 3

---
+++ RevisionDemo/src/stream/Demo3.java
@@ -0,0 +1,14 @@
+package stream;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Demo3 {
+	public static void main(String[] args) {
+		List<Integer> list = new ArrayList<>(Arrays.asList(52,11,33,44,22));
+		System.out.println("Ascending Order");
+		list.stream().sorted()
+		.forEach(System.out::println);
+	}
+}
