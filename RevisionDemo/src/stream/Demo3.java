363d6aab8f0465fd25d5eb694b0518194e2a00bc
sarvesh-0 <sarveshteware651@gmail.com>
2024-10-09 14:40:18 +0530

    Stream Demo 3

--- RevisionDemo/src/stream/Demo3.java
+++ RevisionDemo/src/stream/Demo3.java
@@ -2,6 +2,7 @@ package stream;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 
 public class Demo3 {
@@ -9,6 +10,10 @@ public class Demo3 {
 		List<Integer> list = new ArrayList<>(Arrays.asList(52,11,33,44,22));
 		System.out.println("Ascending Order");
 		list.stream().sorted()
+		.forEach(System.out::println);
+		
+		System.out.println("\nDecending Order");
+		list.stream().sorted(Comparator.reverseOrder())
 		.forEach(System.out::println);
 	}
 }
